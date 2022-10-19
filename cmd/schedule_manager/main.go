67bd95c09d879c7a1382875e81a5f407da0fed2c
Xanonymous <trusaidlin@gmail.com>
2022-10-20 03:04:51 +0800

    feat: init schdeule manager di

--- cmd/schedule_manager/main.go
+++ cmd/schedule_manager/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "go.uber.org/fx"
+import (
+	"github.com/NEO-TAT/tat_auto_roll_call_service/pkg/fb"
+	"github.com/NEO-TAT/tat_auto_roll_call_service/pkg/goredis"
+	"github.com/NEO-TAT/tat_auto_roll_call_service/pkg/logrux"
+	"github.com/NEO-TAT/tat_auto_roll_call_service/src/schedule_manager"
+	"go.uber.org/fx"
+)
 
 func main() {
-	fx.New().Run()
+	fx.New(
+		fx.Provide(
+			logrux.NewLogger,
+			goredis.NewGoRedisClient,
+			fb.CreateFirebaseClients,
+		),
+		fx.Invoke(
+			schedule_manager.CreateManager,
+		),
+	).Run()
 }
