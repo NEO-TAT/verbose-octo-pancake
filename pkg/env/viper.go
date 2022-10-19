b0064b86b352a2b43f784c500679992090566e00
Xanonymous <trusaidlin@gmail.com>
2022-10-20 03:04:26 +0800

    chore: add envs

--- pkg/env/viper.go
+++ pkg/env/viper.go
@@ -1,7 +1,21 @@
 package env
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"strings"
+)
+
+var (
+	IsDebugMode        bool
+	RedisHosts         []string
+	RedisPassword      string
+	FirebaseConfigPath string
+)
 
 func init() {
 	viper.AutomaticEnv()
+	IsDebugMode = viper.GetBool("DEBUG")
+	RedisHosts = strings.Split(viper.GetString("REDIS_HOSTS"), ",")
+	RedisPassword = viper.GetString("REDIS_PASSWORD")
+	FirebaseConfigPath = viper.GetString("FIREBASE_CONFIG_PATH")
 }
